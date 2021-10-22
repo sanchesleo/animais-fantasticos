7cbe73e643c5e285d20df5f6d9d35f826ff17beb
sanchesleo <sanchesleoalves@gmail.com>
2021-10-22 13:12:34 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,9 +1,9 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function initFetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
-    .then((json) => {
-      const btcPreco = document.querySelector('.btc-preco');
-      btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
     }).catch((erro) => {
       console.log(Error(erro));
     });
